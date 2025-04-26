b29ba38b830f01931a66961ad0142981926cc616
Davi0254 <martinsoliveiradavi83@gmail.com>
2025-04-26 11:30:50 -0300

    endpoints da railway adicionados

--- src/servidor/indice.js
+++ src/servidor/indice.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: '*',
+    origin: 'https://mrdaveplataforma-production.up.railway.app',
     credentials: true
 }));
 
--- src/utils/handleLogin.js
+++ src/utils/handleLogin.js
@@ -12,7 +12,7 @@ const handleLogin = async (
     }
 
     try {
-        const response = await fetch("http://localhost:3000/login", {
+        const response = await fetch("https://mrdaveplataforma-production.up.railway.app/login", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",

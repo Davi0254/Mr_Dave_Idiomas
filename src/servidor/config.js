62c7bcb17a49f5bba99dfb3b06fb80fb08ad06cd
Davi0254 <martinsoliveiradavi83@gmail.com>
2025-04-26 06:28:13 -0300

    atualizando credenciais do banco de dados 2

--- src/servidor/config.js
+++ src/servidor/config.js
@@ -5,10 +5,15 @@ import dotenv from 'dotenv';
 dotenv.config({path: '../.env'});
 
 const pool = mysql2.createPool({
-    host: process.env.MARIADB_HOST,
-    user: process.env.MARIADB_USER,
-    password: process.env.MARIADB_PASSWORD,
-    database: process.env.MARIADB_DATABASE,
+    // host: process.env.MARIADB_HOST,
+    // user: process.env.MARIADB_USER,
+    // password: process.env.MARIADB_PASSWORD,
+    // database: process.env.MARIADB_DATABASE,
+
+    host: 'mysql.railway.internal',
+    user: 'davi',
+    password: 'ZdfVcjTOqTCNkbHByfcaQlZnCGJWileW',
+    database: 'mrdave',
 });
 
 export default pool;

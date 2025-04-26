94c5fe8c827acf4941685edff3b33d38a30d6f5b
Davi0254 <martinsoliveiradavi83@gmail.com>
2025-04-26 10:50:02 -0300

    variaveis de ambiente atualizadas

--- src/servidor/config.js
+++ src/servidor/config.js
@@ -2,18 +2,13 @@
 import mysql2 from 'mysql2'
 import dotenv from 'dotenv';
 
-dotenv.config({path: '../.env'});
+// dotenv.config({path: '../.env'});
 
 const pool = mysql2.createPool({
-    // host: process.env.MARIADB_HOST,
-    // user: process.env.MARIADB_USER,
-    // password: process.env.MARIADB_PASSWORD,
-    // database: process.env.MARIADB_DATABASE,
-
-    host: 'mysql.railway.internal',
-    user: 'davi',
-    password: 'ZdfVcjTOqTCNkbHByfcaQlZnCGJWileW',
-    database: 'mrdave',
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQL_DATABASE,
 });
 
 export default pool;

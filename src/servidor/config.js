67876def877ceaddebe5942688f080b9d1b62b51
Davi0254 <martinsoliveiradavi83@gmail.com>
2025-04-26 11:10:28 -0300

    variaveis de ambiente atualizadas 2

--- src/servidor/config.js
+++ src/servidor/config.js
@@ -6,6 +6,7 @@ import dotenv from 'dotenv';
 
 const pool = mysql2.createPool({
     host: process.env.MYSQLHOST,
+    port: process.env.MYSQLPORT,
     user: process.env.MYSQLUSER,
     password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQL_DATABASE,

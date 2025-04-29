c6fc487a4b12feb46dfd4344c27c6d2e09325ba4
Davi0254 <martinsoliveiradavi83@gmail.com>
2025-04-29 18:46:19 -0300

    url cors atualizada 3

--- src/servidor/indice.js
+++ src/servidor/indice.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use(cors({
-    origin: 'https://mrdave.netlify.app/',
+    origin: 'https://mrdave.netlify.app',
     credentials: true
 }));
 

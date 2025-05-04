0de4e78e6a3391adbee584e0334c6010bd080a76
Davi0254 <martinsoliveiradavi83@gmail.com>
2025-05-04 12:29:45 -0300

    urls localhost substituidas pelas do servidor remoto render

--- src/utils/handleRegister.js
+++ src/utils/handleRegister.js
@@ -9,7 +9,7 @@ const handleRegister = async (e, {registerForm, setRegisterForm, setError, setMe
     }
 
     try {
-        const response = await fetch("http://localhost:3000/register", {
+        const response = await fetch("https://mr-dave-plataforma.onrender.com/register", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",

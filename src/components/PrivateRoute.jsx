b56c45aef7c656abbd766577fb578d6b591438b2
Davi0254 <martinsoliveiradavi83@gmail.com>
2025-05-10 13:28:19 -0300

    permissoes de acesso aos cursos alteradas apenas para alunos matriculados, teste

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -13,7 +13,7 @@ export const PrivateRoute = ({ children }) => {
 export const AllowedCourse = ({ children }) => {
     const { cursos } = useAuth();
     const coursePath = location.pathname;
-
+   
     return !cursos?.includes(coursePath) ? alert('operação não permitida') : children
 }
 

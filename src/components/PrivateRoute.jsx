c36279b3c2d64305abbc96e80151e2b4baa95177
Davi0254 <martinsoliveiradavi83@gmail.com>
2025-05-11 11:31:43 -0300

    teste do token jwt

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -11,8 +11,18 @@ export const PrivateRoute = ({ children }) => {
 }
 
 export const AllowedCourse = ({ children }) => {
-    const { cursos } = useAuth();
+    const { cursos, token } = useAuth();
     const coursePath = location.pathname;
+
+    if (loading) {
+        return <div>Loading ...</div>
+    }
+
+    if (token) {
+        return children
+    } else {
+        <Navigate to="/" />
+    }
 
     if (cursos === null) {
         alert('Por favor, recarregue a página novamente')

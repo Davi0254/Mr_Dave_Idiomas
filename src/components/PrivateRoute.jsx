aec192ff4394047ce7689d5fcee8eb3c180de4f9
Davi0254 <martinsoliveiradavi83@gmail.com>
2025-05-10 22:58:47 -0300

    alerta de erro modificado 2

--- src/components/PrivateRoute.jsx
+++ src/components/PrivateRoute.jsx
@@ -14,6 +14,6 @@ export const AllowedCourse = ({ children }) => {
     const { cursos } = useAuth();
     const coursePath = location.pathname;
 
-    return cursos?.includes(coursePath) ? children : alert(`recarregue a pagina e acesse o seu curso ${cursos[0].split('/')[1]}`);
+    return cursos?.includes(coursePath) ? children : alert(`volte a pagina anterior e recarregue a pagina para acessar o seu curso ${cursos[0].split('/')[1]}`);
 }
 

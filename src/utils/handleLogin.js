bb9344479b49acd31ec619e43e7dfc61561c5c1a
Davi0254 <martinsoliveiradavi83@gmail.com>
2025-05-28 10:37:04 -0300

    database alterada temporariamente para localhost

--- src/utils/handleLogin.js
+++ src/utils/handleLogin.js
@@ -14,7 +14,7 @@ const handleLogin = async (
 // Remorte server url: https://mr-dave-plataforma.onrender.com
 
     try {
-        const response = await fetch("https://mr-dave-plataforma.onrender.com/login", {
+        const response = await fetch("https://mr-dave-plataforma.onrender.com", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json",
